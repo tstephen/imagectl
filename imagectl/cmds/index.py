beb307e091fabb12cd93ed452beb1353c88bf71a
Tim Stephenson <tim@knowprocess.com>
2024-01-07 20:02:53 +0000

    feat: index a dir

--- imagectl/cmds/index.py
+++ imagectl/cmds/index.py
@@ -16,15 +16,39 @@
 # Command line client for managing an image library.
 #
 ###############################################################################
+import logging
+import os
+from os.path import join, getctime, getmtime, getsize
+
 from imagectl.api import ImageCommand, ImageCommandOptions
+from imagectl.constants import TOOL
+from imagectl.models import IndexEntry
+
+logger = logging.getLogger(__name__)
 
 class IndexCommand(ImageCommand):
     """Command to index an image library"""
     NAME = 'index'
+    valid_extensions = [".jpg", ".jpeg", ".png"]
+
     def __init__(self, subparsers):
         super().__init__(self)
         self.cmd = subparsers.add_parser(self.NAME,
                    help='index an image library')
+        self.cmd.add_argument("-i", "--input", help="base image directory")
 
     def execute(self, options: ImageCommandOptions):
-        print(f'command {self.NAME} not yet implemented')
+        logger.setLevel(options.verbose)
+        logger.info("indexing %s", options.input)
+
+        index = []
+        for root, dirs, files in os.walk(options.input):
+            for name in files:
+                qualified_name = join(root, name)
+                index.append(IndexEntry(name=qualified_name[len(options.input)+1:],
+                                        created=getctime(qualified_name),
+                                        modified=getmtime(qualified_name),
+                                        size=getsize(qualified_name)))
+        with open(join(options.input, f'.{TOOL.get("name")}'), 'w') as out:
+            for entry in index:
+                out.write(entry.model_dump())
