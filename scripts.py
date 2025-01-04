a380e6bb82f315a75cc2375a22825ec1699122c2
Tim Stephenson <tim@knowprocess.com>
2025-01-04 18:34:12 +0000

    fix: run tests

--- scripts.py
+++ scripts.py
@@ -4,10 +4,10 @@ Project lifecycle scripts defined in pyproject.toml
 import subprocess
 
 def tests():
-    """
-    Run all unittests. Equivalent to:
-    `poetry run coverage run --omit */site-packages/*,tests/* -m pytest tests/`
-    """
+    """ Run all unit tests and report test coverage """
     subprocess.run(
         ['coverage', 'run', '--omit', '*/site-packages/*,tests/*', '-m', 'pytest', 'tests/']
+    )
+    subprocess.run(
+        ['coverage', 'report']
     )
